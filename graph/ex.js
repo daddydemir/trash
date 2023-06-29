e00b2f8778e1b9b49480d96a261b07f3f2054391
daddydemir <42716195+daddydemir@users.noreply.github.com>
2023-06-29 21:57:02 +0300

    update: chart is updated.

--- graph/ex.js
+++ graph/ex.js
@@ -1,72 +1,15 @@
-const getData = async () => {
-
-    let array;
-    await fetch(
-        await getUrl() + "/getDaily",
-        {
-            method: 'post',
-            headers: {
-                'Accept': 'text/plain',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "startDate": "2023-06-22",
-                "endDate": "2023-06-30",
-            }),
-        }
-    ).then(response => {
-        return response.json();
-    }).then(function (data) {
-        array = data;
-        array.sort(function (a, b) {
-            return b.Avg - a.Avg
-        })
-    });
-    return array;
+let exchanges = [];
+let exchangeMap = {};
+let colors = {
+    'pink': '#ff6384'
+    , 'blue': '#36a2eb'
+    , 'purple': '#cc65fe'
+    , 'yellow': '#ffce56'
+    , 'black': '#000'
 }
+let days = ['pazartesi', 'salı', 'çarşamba', 'perşembe', 'cuma', 'cumartesi', 'pazar'];
+let myChart = null;
 
-
-const createGraph = async () => {
-    let arr = await getData();
-    let data = {
-        labels: ['min', 'avg', 'max'],
-        datasets: [],
-    };
-
-    for (let i = 10; i < 17; i++) {
-        let item = {
-            label: arr[i].ExchangeId,
-            data: [arr[i].Min, arr[i].Avg, arr[i].Max],
-            backgroundColor: [
-                '#ff6384',
-                '#36a2eb',
-                '#cc65fe',
-                '#ffce56',
-            ],
-        }
-        data.datasets.push(item);
-
-    }
-
-    return data;
-}
-
-const draw = async () => {
-    let arr = await createGraph();
-
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: arr,
-        options: {
-            plugins: {
-                colors: {
-                    enabled: false
-                }
-            }
-        }
-    });
-}
 
 const refreshExchanges = async () => {
     let btn = document.getElementById('refreshBtn');
@@ -86,6 +29,8 @@ const refreshExchanges = async () => {
         return response.json();
     }).then(function (data) {
         array = data;
+        exchanges = data;
+        normalize(array);
         btn.classList.remove('fa-spin')
     });
     await exchangeCreator(array);
@@ -94,6 +39,7 @@ const refreshExchanges = async () => {
 
 const exchangeCreator = async (array) => {
     let select = document.getElementById('exchanges');
+    select.innerHTML = '';
     for (let i = 0; i < array.length; i++) {
         let o = document.createElement('option');
         o.text = array[i].Name;
@@ -101,4 +47,103 @@ const exchangeCreator = async (array) => {
         select.add(o);
     }
 
+}
+
+const getWeekly = async () => {
+    let startDate = document.getElementById('startDate').value;
+    let endDate = document.getElementById('endDate').value;
+    let id = document.getElementById('exchanges').value;
+    let out = [];
+
+    startDate = '2023-06-01';
+    endDate = '2023-06-30';
+
+    await fetch(
+        await getUrl() + '/getDailyWithId',
+        {
+            method: 'post',
+            headers: {
+                'Accept': 'text/plain',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                startDate: startDate,
+                endDate: endDate,
+                id: id,
+            }),
+        },
+    ).then(response => {
+        return response.json();
+    }).then(data => {
+        out = data;
+    });
+
+
+    let image = document.getElementById('coin-img');
+    image.src = exchangeMap[id].CoinImage;
+    let name = document.getElementById('coin-name');
+    name.textContent = exchangeMap[id].Name;
+
+    barGraph(out);
+
+
+}
+
+const normalize = async (array) => {
+    for (let i = 0; i < array.length; i++) {
+        exchangeMap[array[i].ExchangeId] = array[i];
+    }
+}
+
+
+const barGraph = (arr) => {
+
+    let data = {
+        labels: days,
+        datasets: [],
+    };
+    let mins = [];
+    let maxs = [];
+    let avgs = [];
+    for (let i = 0; i < arr.length; i++) {
+        mins[i] = arr[i].Min;
+        maxs[i] = arr[i].Max;
+        avgs[i] = arr[i].Avg;
+    }
+
+    let min = {
+        label: arr[0].ExchangeId + '-min',
+        data: mins,
+        backgroundColor: [colors['blue']]
+    };
+    let avg = {
+        label: arr[0].ExchangeId + '-avg',
+        data: avgs,
+        backgroundColor: [colors['pink']],
+    };
+    let max = {
+        label: arr[0].ExchangeId + '-max',
+        data: maxs,
+        backgroundColor: [colors['yellow']],
+    };
+
+
+    data.datasets.push(min);
+    data.datasets.push(avg);
+    data.datasets.push(max);
+    let canvas = document.getElementById('myChart');
+    if (myChart !== null) {
+        myChart.destroy();
+    }
+    //canvas.remove();
+    //let div = document.getElementById('div-chart');
+    //div.append(`<canvas id="myChart"> </canvas>`);
+    //canvas = document.getElementById('myChart');
+
+    var ctx = canvas.getContext('2d');
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {}
+    });
 }
